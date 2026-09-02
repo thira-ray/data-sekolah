e95eed9ca8d27bfd0a9ce1a0385082c918a82051
thira-ray <nuraisyahathira04@gmail.com>
2026-09-02 12:55:39 +0800

    Create schools.js

---
+++ schools.js
@@ -0,0 +1,25 @@
+window.SCHOOLS = {
+  "DBA0001": {
+    nama: "SEK KEB ALOR BAKAT",
+    daerah: "BACHOK",
+    negeri: "KELANTAN"
+  },
+
+  "DBA0002": {
+    nama: "SEK KEB BACHOK",
+    daerah: "BACHOK",
+    negeri: "KELANTAN"
+  },
+
+  "DBA0003": {
+    nama: "SEK KEB BADAK",
+    daerah: "BACHOK",
+    negeri: "KELANTAN"
+  },
+
+  "DBA0004": {
+    nama: "SEK KEB BAKONG",
+    daerah: "BACHOK",
+    negeri: "KELANTAN"
+  }
+};
